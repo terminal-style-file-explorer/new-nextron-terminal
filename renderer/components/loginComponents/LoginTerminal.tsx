142f1ca624b0fcd157b8dca5df8bcc66141bd9bc
lufor <lufor_most_cute@163.com>
2024-03-27 07:14:20 +0800

    增加 login页面的handleSubmit后进行分析 缺少后端和fetch方法 WIP

--- renderer/components/loginComponents/LoginTerminal.tsx
+++ renderer/components/loginComponents/LoginTerminal.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { useEffect, useRef } from 'react';
 import _ from 'lodash';
-import { Form, Input, User, Wrapper } from '../styles/terminal.styled';
+import { CmdNotFound, Empty, Form, Input, User, Wrapper } from '../styles/terminal.styled';
 
 type User = {
   name: string;
@@ -8,15 +9,84 @@ type User = {
   auth: number;
 };
 
+type Command = {
+  cmd: string;
+  desc: string;
+  tab: number;
+}[];
+
+
+
+export const commands: Command = [
+  { cmd: "addUser", desc: "create an account", tab: 6 },
+  { cmd: "su", desc: "change user", tab: 11 },
+];
+
 export default function LoginTerminal() {
   const [inputValue, setInputValue] = React.useState('');
   const [cmdHistory, setCmdHistory] = React.useState<string[]>([]);
-  const [resultHistory, setResultHistory] = React.useState<string[]>([]);
+  const [resultHistory, setResultHistory] = React.useState<JSX.Element[]>([]);
   const inputRef = React.useRef<HTMLInputElement>(null);
+  const containerRef = React.useRef<HTMLDivElement>(null);
 
   const [user, setUser] = React.useState<User>(
     { name: 'visitor', password: '000000', auth: 0 }
   );
+  function checkUser(user: User) {
+    return true;
+
+  }
+  function addUser(user: User) {
+    return true;
+  
+  }
+
+  const cmdParse = (cmd: string) => {
+    let cmdArray = _.split(_.trim(cmd), ' ');
+    let validCmd = _.find(commands, { cmd: cmdArray[0] });
+    let currentCmd = cmdArray[0];
+    if (cmd === '') {
+      return <Empty />;
+    }
+    else if (validCmd) {
+      if (cmdArray.length === 3) {
+        let userToCheck = { name: cmdArray[1], password: cmdArray[2], auth: 0 };
+        switch (currentCmd) {
+          case "addUser":
+            if (addUser(userToCheck)) {
+              // 库中不存在对应用户名，可以新建用户
+              return <div>user added</div>
+            }
+            else {
+              return <div>user already exists</div>
+            }
+            break;
+          case "su":
+            if (checkUser(userToCheck)) {
+              // 用户存在，可以切换用户
+              return <div>user changed</div>
+            }
+            else {
+              return <div>user not found</div>
+            }
+            break;
+        }
+      }
+      else {
+        return <CmdNotFound>
+          invalid command: {cmd}
+        </CmdNotFound>
+      }
+    }
+    else {
+      return <CmdNotFound>
+        command not found: {cmd}
+      </CmdNotFound>
+    }
+  }
+
+
+
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
@@ -24,13 +94,30 @@ export default function LoginTerminal() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setCmdHistory([...cmdHistory, inputValue]);
-    setResultHistory([...resultHistory, inputValue]);
+    setResultHistory([...resultHistory, cmdParse(inputValue)]);
     setInputValue('');
   }
 
+  // For caret position at the end
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      inputRef?.current?.focus();
+    }, 1);
+    return () => clearTimeout(timer);
+  }, [inputRef, inputValue]);
 
+
+  const handleDivClick = () => {
+    inputRef.current && inputRef.current.focus();
+  };
+  useEffect(() => {
+    document.addEventListener("click", handleDivClick);
+    return () => {
+      document.removeEventListener("click", handleDivClick);
+    };
+  }, [containerRef]);
   return (
-    <Wrapper >
+    <Wrapper ref={containerRef}>
       <Form onSubmit={handleSubmit} className='flex'>
         <label>
           <User>{user.name + "@: "}</User>
@@ -51,7 +138,8 @@ export default function LoginTerminal() {
             <div key={_.uniqueId(`${cmdH}_`)}>
               <div key={index}>
                 <div id='terminal-info'>
-                  <User>{user.name + "@: "}</User>
+                  <User>{user.name}</User>
+                  <span>@: {cmdH}</span>
                 </div>
                 <div id='terminal-output'>
                   {resultHistory[index]}
